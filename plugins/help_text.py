4f6ccbd81cfb92f17dc912c6cb54d54753357668
X Roid <97869723+XRoiDX@users.noreply.github.com>
2022-01-19 18:27:59 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -52,11 +52,11 @@ async def start(bot, update):
             [
                 [
                     InlineKeyboardButton(
-                        "Source code ⚡", url="https://github.com/Clinton-Abraham/UPLOADER-BOT"
+                        "Gɪᴛʜᴜʙ Pʀᴏꜰɪʟᴇ⚡", url="https://github.com/XRoiDX"
                     ),
-                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/Space_X_bots"),
+                    InlineKeyboardButton("Pʀᴏᴊᴇᴄᴛ Cʜᴀɴɴᴇʟ 👨🏻‍💻", url="https://t.me/XRoid_BotZ"),
                 ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham_bot")],
+                [InlineKeyboardButton("Dᴇᴠᴇʟᴏᴘᴇʀ 👨‍⚖️", url="https://t.me/Space4AFML")],
             ]
         ),
         reply_to_message_id=update.message_id
