0313eb9e6a36b2d8df3bba70c8f2a6010d646a94
X Roid <97869723+XRoiDX@users.noreply.github.com>
2022-01-30 08:43:09 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -18,5 +18,5 @@ async def sts(c, m):
 @Clinton.on_message(filters.private & filters.command("search"))
 async def serc(bot, update):
 
-      await bot.send_message(chat_id=update.chat.id, text="🔍 TORRENT SEARCH", 
+      await bot.send_message(chat_id=update.chat.id, text="🔍 TORRENT SEARCH ©XROID", 
       parse_mode="html", reply_markup=Button.BUTTONS01)
